cb4defef30a0cc0aa5f46a70d7f76ca8b39cf136
DanRev <dbeltrangomez@gmail.com>
2019-02-14 00:40:48 -0500

    Cambios Expresion Cadena
    Se cambio la expresion cadena

--- src/sintaxis/ExpresionCadena.java
+++ src/sintaxis/ExpresionCadena.java
@@ -2,12 +2,61 @@ package sintaxis;
 
 import javax.swing.tree.DefaultMutableTreeNode;
 
-public class ExpresionCadena extends Expresion{
+/**
+ * Clase que representa la expresion Cadena
+ * 
+ * @author Daniel Beltran Gomez
+ * @author Tatiana Salazar
+ * @author Juan Jose alvarez
+ *
+ */
+
+public class ExpresionCadena extends Expresion {
+
+	private Termino termino;
+	private ExpresionCadena expCadena;
+
+	/**
+	 * Constructor para una expresion de cadena con solo un termino
+	 * 
+	 * @param termino
+	 */
+	public ExpresionCadena(Termino termino) {
+		super();
+		this.termino = termino;
+	}
+
+	/**
+	 * Constructor para una expresion de cadena con uno o mas terminos
+	 * 
+	 * @param termino
+	 * @param expCadena
+	 */
+	public ExpresionCadena(Termino termino, ExpresionCadena expCadena) {
+		super();
+		this.termino = termino;
+		this.expCadena = expCadena;
+	}
 
 	@Override
 	public DefaultMutableTreeNode getArbolVisual() {
-		// TODO Auto-generated method stub
-		return null;
+
+		DefaultMutableTreeNode nodo = new DefaultMutableTreeNode("Expresion Cadena");
+
+		nodo.add(termino.getArbolVisual());
+		if (expCadena != null) {
+			nodo.add(expCadena.getArbolVisual(nodo));
+		}
+		return nodo;
+	}
+
+	public DefaultMutableTreeNode getArbolVisual(DefaultMutableTreeNode nodo) {
+
+		nodo.add(termino.getArbolVisual());
+		if (expCadena != null) {
+			nodo.add(expCadena.getArbolVisual(nodo));
+		}
+		return nodo;
 	}
 
 }
