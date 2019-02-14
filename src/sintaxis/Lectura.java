4fe56a3259bf5bdb318828bb199d30d647f92315
DanRev <dbeltrangomez@gmail.com>
2019-02-14 01:07:58 -0500

    Cambios en Lectura

--- src/sintaxis/Lectura.java
+++ src/sintaxis/Lectura.java
@@ -2,12 +2,55 @@ package sintaxis;
 
 import javax.swing.tree.DefaultMutableTreeNode;
 
-public class Lectura extends Sentencia{
+import lexico.Token;
+
+public class Lectura extends Sentencia {
+
+	private Token idVariable;
+	private Token opAsignacion;
+	private Token leer;
+	private Token parentesisIzq;
+	private TipoDato tipoDato;
+	private Token parentesisDer;
+	private Token fin;
+
+	/**
+	 * Constructor para leer un dato
+	 * 
+	 * @param idVariable
+	 * @param opAsignacion
+	 * @param leer
+	 * @param parentesisIzq
+	 * @param tipoDato
+	 * @param parentesisDer
+	 * @param fin
+	 */
+	public Lectura(Token idVariable, Token opAsignacion, Token leer, Token parentesisIzq, TipoDato tipoDato,
+			Token parentesisDer, Token fin) {
+		super();
+		this.idVariable = idVariable;
+		this.opAsignacion = opAsignacion;
+		this.leer = leer;
+		this.parentesisIzq = parentesisIzq;
+		this.tipoDato = tipoDato;
+		this.parentesisDer = parentesisDer;
+		this.fin = fin;
+	}
 
 	@Override
 	public DefaultMutableTreeNode getArbolVisual() {
-		// TODO Auto-generated method stub
-		return null;
+
+		DefaultMutableTreeNode nodo = new DefaultMutableTreeNode("Lectura");
+		nodo.add(new DefaultMutableTreeNode(idVariable.getLexema()));
+		nodo.add(new DefaultMutableTreeNode(opAsignacion.getLexema()));
+		nodo.add(new DefaultMutableTreeNode(leer.getLexema()));
+		nodo.add(new DefaultMutableTreeNode(parentesisIzq.getLexema()));
+		if (tipoDato != null) {
+			nodo.add(tipoDato.getArbolVisual());
+			nodo.add(new DefaultMutableTreeNode(parentesisDer.getLexema()));
+			nodo.add(new DefaultMutableTreeNode(fin.getLexema()));
+		}
+		return nodo;
 	}
 
 }
--- src/sintaxis/TipoDato.java
+++ src/sintaxis/TipoDato.java
@@ -1,5 +1,12 @@
 package sintaxis;
 
+import javax.swing.tree.DefaultMutableTreeNode;
+
 public class TipoDato {
+
+	public DefaultMutableTreeNode getArbolVisual() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
